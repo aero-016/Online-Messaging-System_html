cd8a9558b6533310ffd269ae1418158bfaf41880
Vishant Vaghani <58165342+aero-016@users.noreply.github.com>
2020-08-09 23:46:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function validation_form() {
+  var x = document.forms["login1"]["uname"].value;
+  if (x === "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+function validate_cre() {
+  var username = document.forms["login1"]["uname"].value;
+  var password = document.forms["login1"]["psw"].value;
+  document.write(username);
+  if (username === "user1" && password === "user1") {
+    // Check browser support
+   
+    window.location.replace("main.html");
+     if (typeof (Storage) !== "undefined") {
+      // Store
+      sessionStorage.setItem("username",username);
+      // Retrieve
+      document.getElementById("self").innerHTML = sessionStorage.getItem("username");
+    } else {
+      document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+  }
+  else {
+    window.location.replace("index.html");
+  }
+}
