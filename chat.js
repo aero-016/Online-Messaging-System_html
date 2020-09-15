64b164713719ce5d48d7fd9e43eaa7a562dccd7a
Vishant Vaghani <58165342+aero-016@users.noreply.github.com>
2020-09-15 23:46:01 +0530

    Add files via upload

--- chat.js
+++ chat.js
@@ -3,8 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$(document).ready(function(){
-$('.friend-drawer').click(function(){
-   $('.chat-box--tray').hide('slow').show('slow'); 
+            $(document).ready(function(){
+$('.friend-drawer--onhover').click(function(){
+   $('#chat').hide('slow').show('slow');
 });
+
+    $("#message,#friend").click(function(){
+         var chatHistory = document.getElementById("chat");
+        chatHistory.scrollTop = chatHistory.scrollHeight;
+    });
+ 
+ 
+ $('#message').keypress(function(event){
+ var keycode = (event.keyCode ? event.keyCode : event.which);
+ if(keycode == '13'){
+    // alert('You pressed a "enter" key in textbox');
+    // var children = document.getElementById( "chat-message" );
+     $('#chat-message').append('<div class="row float-right"><div class="col-sm-6"><div class="chat-box--right" style="">'+$('#message').val()+'</div></div></div><br><br>');
+    // console.log(children);
+     var chatHistory = document.getElementById("chat");
+        chatHistory.scrollTop = chatHistory.scrollHeight;
+        
+      $('#message').val("");  
+ }
+  event.stopPropagation();
+ });
+ 
+   $("#search").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#friends *").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+ 
+            
 });
+ 
